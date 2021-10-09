3a2d82aa62106892e9758f69f9adee1945a49d1f
Jeffrey Geer <viriveri@gmail.com>
2021-10-09 05:08:46 -0500

    Added option for Admin Auto-Launch items to use the Task Scheduler
    Doing this allows for the item to be killed when the game closes via "schtasks.exe". This also stops the Admin prompt when starting the task. If something with the Scheduled Task goes wrong, it'll log it and fall back to the normal Run as Admin logic.

--- src/XIVLauncher/Windows/GenericAddonSetupWindow.xaml.cs
+++ src/XIVLauncher/Windows/GenericAddonSetupWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using CheapLoc;
+using System.Windows;
 using XIVLauncher.Addon;
 using XIVLauncher.Windows.ViewModel;
 
@@ -22,9 +23,12 @@ namespace XIVLauncher.Windows
                 PathEntry.Text = addon.Path;
                 CommandLineTextBox.Text = addon.CommandLine;
                 AdminCheckBox.IsChecked = addon.RunAsAdmin;
+                SchTaskCheckBox.IsChecked = addon.UseSchTask;
                 RunOnCloseCheckBox.IsChecked = addon.RunOnClose;
                 KillCheckBox.IsChecked = addon.KillAfterClose;
             }
+
+            SchTaskCheckBox.IsEnabled = AdminCheckBox.IsChecked ?? false;
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
@@ -37,6 +41,7 @@ namespace XIVLauncher.Windows
                 Path = PathEntry.Text,
                 CommandLine = CommandLineTextBox.Text,
                 RunAsAdmin = AdminCheckBox.IsChecked == true,
+                UseSchTask = SchTaskCheckBox.IsChecked == true,
                 RunOnClose = RunOnCloseCheckBox.IsChecked == true,
                 KillAfterClose = KillCheckBox.IsChecked == true
             };
@@ -46,13 +51,29 @@ namespace XIVLauncher.Windows
 
         private void AdminCheckBox_OnChecked(object sender, RoutedEventArgs e)
         {
+            SchTaskCheckBox.IsEnabled = true;
+            SchTaskCheckBox.IsChecked = false;
             KillCheckBox.IsEnabled = false;
             KillCheckBox.IsChecked = false;
         }
 
         private void AdminCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
         {
+            SchTaskCheckBox.IsEnabled = false;
+            SchTaskCheckBox.IsChecked = false;
             KillCheckBox.IsEnabled = true;
+        }
+
+        private void SchTaskCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            KillCheckBox.IsEnabled = true;
+            KillCheckBox.IsChecked = true;
+        }
+
+        private void SchTaskCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            KillCheckBox.IsEnabled = false;
+            KillCheckBox.IsChecked = false;
         }
     }
 }
